93398ecbc878b733839ef3af5170437352d90283
Andrea Hoyt <ahoyt@mathspace.com.au>
2023-11-15 12:53:53 -0600

    Update script.js

--- wom-skills-POC/script.js
+++ wom-skills-POC/script.js
@@ -12,55 +12,64 @@ function displayGrid(result) {
     const gridContainer = document.getElementById('grid-container');
     let currentStrand = '';
     let currentTopic = '';
-    let topicDiv, list, strandContent;
+    let topicDiv, list;
+    let strandCounter = 0;
+    let topicCounter = 0;
 
     data.forEach(row => {
-        // Check if the strand has changed
         if (row.Strand !== currentStrand) {
             currentStrand = row.Strand;
+            strandCounter++;
+            topicCounter = 0; // Reset topic counter for each new strand
+
             const strandHeader = document.createElement('h2');
             strandHeader.className = 'strand-header collapsible';
             strandHeader.textContent = currentStrand;
+            strandHeader.setAttribute('data-strand', strandCounter);
 
-            strandContent = document.createElement('div');
-            strandContent.className = 'content'; 
+            const strandContent = document.createElement('div');
+            strandContent.className = 'content';
+            strandContent.setAttribute('data-strand-content', strandCounter);
 
             strandHeader.addEventListener('click', function() {
-                this.classList.toggle("active");
-                strandContent.style.display = strandContent.style.display === 'none' ? '' : 'none';
+                const targetContent = document.querySelector(`[data-strand-content="${this.getAttribute('data-strand')}"]`);
+                targetContent.style.display = targetContent.style.display === 'none' ? '' : 'none';
             });
 
             gridContainer.appendChild(strandHeader);
             gridContainer.appendChild(strandContent);
         }
 
-        // Check if the topic has changed
         if (row.topic_title !== currentTopic) {
             currentTopic = row.topic_title;
+            topicCounter++;
             topicDiv = document.createElement('div');
             topicDiv.className = 'topic-container content';
+            topicDiv.setAttribute('data-topic', topicCounter);
 
             const topicHeader = document.createElement('h3');
             topicHeader.className = 'topic-subheader collapsible';
             topicHeader.textContent = currentTopic;
+            topicHeader.setAttribute('data-topic', topicCounter);
 
             list = document.createElement('ul');
             list.className = 'subtopic-list';
 
             topicHeader.addEventListener('click', function() {
-                this.classList.toggle("active");
-                list.style.display = list.style.display === 'none' ? '' : 'none';
+                const targetList = document.querySelector(`[data-topic="${this.getAttribute('data-topic')}"] .subtopic-list`);
+                targetList.style.display = targetList.style.display === 'none' ? '' : 'none';
             });
 
             topicDiv.appendChild(topicHeader);
             topicDiv.appendChild(list);
-            strandContent.appendChild(topicDiv);
+            const targetStrandContent = document.querySelector(`[data-strand-content="${strandCounter}"]`);
+            targetStrandContent.appendChild(topicDiv);
         }
 
-        // Add subtopic as a list item
         const listItem = document.createElement('li');
         listItem.className = 'subtopic-item';
         listItem.textContent = row.subtopic_title;
         list.appendChild(listItem);
     });
 }
+
